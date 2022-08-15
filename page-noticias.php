9c91d4fd0291472a948ac0e1c69a08eebd2f7320
unknown <lenomoraes12@gmail.com>
2022-08-15 19:01:53 -0400

    adicionado varios arquivos 4

--- page-noticias.php
+++ page-noticias.php
@@ -17,9 +17,43 @@ Template Name: Page Noticias
                 
                 <?php
 
-                    the_content();         
+                    the_content();                                                           
                                     
                 ?>
+
+                    <?php
+
+                    $args = array(
+                        'posts_per_page' => 3
+                    );
+
+                    $news_query = new WP_Query( $args );
+
+                    if ( $news_query->have_posts() ):
+                        while ($news_query->have_posts()) :
+                            $news_query->the_post();
+                    ?>
+                        <article>
+                            <a href="<?php the_permalink(); ?>" style="text-decoration: none;" ><h3><?php the_title(); ?></h3></a>
+                            <a href="<?php the_permalink(); ?>"> <?php the_post_thumbnail( 'medium', array( 'loading' => '' ) ); ?> </a>
+                            <div>
+                                <p>Postado em <?php echo get_the_date(); ?> por <?php the_author_posts_link(); ?></p>
+                            </div>
+                            <?php the_excerpt(); 
+                            ?>
+                        </article>
+                    <?php
+                        endwhile;
+
+                        the_posts_pagination(array(
+                        ));
+
+                    else:
+                    ?>
+                        <p>Não há posts</p>
+                    <?php
+                    endif;
+                    ?>                
 
             </div>
         </div>
